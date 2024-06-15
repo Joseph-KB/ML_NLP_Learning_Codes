73e20d710052864d834ff6e053fecfe70aa74c80
JosephKB <kbjoseph2@gmail.com>
2024-06-15 17:16:34 +0530

    Inital setup Udemy course

---
+++ venv_vscode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ~/Desktop/Obsidian-1.4.16-arm64.AppImage &&
+echo "Complete Machine Learning NLP Bootcamp MLOPS and Deployment by KRISH NAIK via UDEMY"
+. ./venv/bin/activate
+code .
