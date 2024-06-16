88629f0916734aea14975ea2c593cec4fb4da1a5
Joseph K B <kbjoseph2@gmail.com>
2024-06-16 17:39:17 +0530

    updated shell script

--- venv_vscode.sh
+++ venv_vscode.sh
@@ -2,5 +2,16 @@
 
 # ~/Desktop/Obsidian-1.4.16-arm64.AppImage &&
 echo "Complete Machine Learning NLP Bootcamp MLOPS and Deployment by KRISH NAIK via UDEMY"
-. ./venv/bin/activate
+echo "------------$OSTYPE------------"
+read -p "Press Enter to continue..."
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" =="win32" ]]; then
+    echo "Activating VENV on windows machine"
+    . ./venv/Scripts/activate
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Activating VENV on linux machine"
+    . ./venv/bin/activate
+else
+    echo "Unsupported OS : $OSTYPE"
+fi
+x
 code .
