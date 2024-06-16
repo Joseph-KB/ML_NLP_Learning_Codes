bb6d48b6dd7868125fa4f2521ab15fee5388e210
Joseph K B <kbjoseph2@gmail.com>
2024-06-16 18:03:21 +0530

    Correction in Shell Script

--- venv_vscode.sh
+++ venv_vscode.sh
@@ -2,16 +2,17 @@
 
 # ~/Desktop/Obsidian-1.4.16-arm64.AppImage &&
 echo "Complete Machine Learning NLP Bootcamp MLOPS and Deployment by KRISH NAIK via UDEMY"
-echo "------------$OSTYPE------------"
-read -p "Press Enter to continue..."
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" =="win32" ]]; then
+echo "-----OSTYPE-------$OSTYPE------------"
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     echo "Activating VENV on windows machine"
-    . ./venv/Scripts/activate
+    source ./venv/Scripts/activate
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Activating VENV on linux machine"
-    . ./venv/bin/activate
+    source ./venv/bin/activate
 else
     echo "Unsupported OS : $OSTYPE"
+    read -p "Only supported is Windows and Linux Press Enter to continue..."
 fi
 
 code .
