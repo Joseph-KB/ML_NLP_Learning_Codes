5185c3463b2dfd9e604c9910f0c626204b0e75ae
JosephKB <kbjoseph2@gmail.com>
2024-06-30 12:21:08 +0530

    venv shell script python version changed

--- venv_vscode.sh
+++ venv_vscode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VIRTUALEN_FILE_NAME="venv"
-PYTHON_VERSION="python3.11"
+VIRTUALENV_FILE_NAME="venv"
+PYTHON_VERSION="python3"
 
 echo "Complete Machine Learning NLP Bootcamp MLOPS and Deployment by KRISH NAIK via UDEMY"
 echo "-----OSTYPE-------$OSTYPE------------"
@@ -10,17 +10,17 @@ create_virtualenv(){
     echo "Pip installing virtualenv if not installed"
     pip install --user "virtualenv==20.26.2"
     echo "Creating Virtualenv PYTHON --VERSION $PYTHON_VERSION"
-    virtualenv $VIRTUALEN_FILE_NAME -p "$PYTHON_VERSION"
+    virtualenv $VIRTUALENV_FILE_NAME -p "$PYTHON_VERSION"
 }
 
 activate_venv_windows(){
     echo "Activating VENV on windows machine"
-    source ./$VIRTUALEN_FILE_NAME/Scripts/activate
+    source ./$VIRTUALENV_FILE_NAME/Scripts/activate
 }
 
 activate_venv_linux(){
     echo "Activating VENV on linux machine"
-    source ./$VIRTUALEN_FILE_NAME/bin/activate
+    source ./$VIRTUALENV_FILE_NAME/bin/activate
 }
 
 pip_install_requirements(){
@@ -29,14 +29,14 @@ pip_install_requirements(){
 }
 
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    if [[ ! -d "$VIRTUALEN_FILE_NAME" ]]; then
+    if [[ ! -d "$VIRTUALENV_FILE_NAME" ]]; then
         create_virtualenv
     fi
     activate_venv_windows
     pip_install_requirements
 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if [[ ! -d "$VIRTUALEN_FILE_NAME" ]]; then
+    if [[ ! -d "$VIRTUALENV_FILE_NAME" ]]; then
         create_virtualenv
     fi
     activate_venv_linux
